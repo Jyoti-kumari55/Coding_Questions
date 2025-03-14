2bd70444832ed2a87791e75a236ea18015b6eddb
Jyoti Kumari <68436635+Jyoti-kumari55@users.noreply.github.com>
2025-03-14 20:59:17 +0530

    oauth

---
+++ Node-Js/OAUTH/oauth-backend/services/index.js
@@ -0,0 +1,13 @@
+//Oauth Security
+
+function setSecureCookie(res, token) {
+    res.cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 60 * 1000,
+
+    });
+
+    return res;
+}
+
+module.exports = { setSecureCookie };
